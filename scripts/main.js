f4ea4d48ed47a968290605ffca4a2e4a54a0d2c2
cedric karungu <83704005+Cedric921@users.noreply.github.com>
2022-06-22 07:39:10 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,47 @@
 const btnValidate = document.getElementById('btn-validate');
-let round = 5;
-let win;
-const toGuess = Math.floor(Math.random() * 10 + 1);
+const btnReset = document.querySelector('.reset');
+const main = document.querySelector('body');
 
-const jouer = () => {
-	const user = document.getElementById('userInput');
-	const message = document.getElementById('message');
-	let userInput = user.value;
-	userInput = parseInt(userInput);
-	if (isNaN(userInput)) {
-		message.textContent = 'entrer un nombre stp';
-		return;
-	} else {
-		if (userInput > toGuess) {
-			message.textContent = 'Your number is to big';
-			round--;
-		} else if (userInput < toGuess) {
-			message.textContent = 'Your number is too small';
-			round--;
+const game = () => {
+	let round = 5;
+	let win;
+	const toGuess = Math.floor(Math.random() * 10 + 1);
+
+	const jouer = () => {
+		const user = document.getElementById('userInput');
+		const message = document.getElementById('message');
+		let userInput = user.value;
+		userInput = parseInt(userInput);
+		if (isNaN(userInput)) {
+			message.textContent = 'entrer un nombre stp';
+			return;
 		} else {
-            win = true;
-            round = 0;
-        }
-        message.innerHTML += `<br> vous avez encore ${round} tentatives`
-	}
-	if (round <= 0) {
-		if (win) {
-            		message.textContent = 'You won, number was ' + toGuess;
-            		message.style.color = 'green';
-		} else {
-            		message.textContent = 'You lost, number was ' + toGuess;
-            		message.style.color = 'red';
+			if (userInput > toGuess) {
+				message.textContent = 'Your number is to big';
+				round--;
+			} else if (userInput < toGuess) {
+				message.textContent = 'Your number is too small';
+				round--;
+			} else {
+		    win = true;
+		    round = 0;
 		}
-	}
-};
+		message.innerHTML += `<br> vous avez encore ${round} tentatives`
+		}
+		if (round <= 0) {
+			if (win) {
+				message.textContent = 'You won, number was ' + toGuess;
+				message.style.color = 'green';
+			} else {
+				message.textContent = 'You lost, number was ' + toGuess;
+				message.style.color = 'red';
+			}
+		}
+	};
+	btnValidate.addEventListener('click', jouer);
+}
 
-btnValidate.addEventListener('click', jouer);
+main.addEventListener('load', game);
+btnReset.addEventListener('click', game);f
+
+
